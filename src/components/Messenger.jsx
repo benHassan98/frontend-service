5482185f996e5950357916fe6f6827798e96559a
Abdullah Mahmoud <abdullahH.Mahmoud98@gmail.com>
2024-05-05 08:59:59 +0200

    Update Messenger.jsx

--- src/components/Messenger.jsx
+++ src/components/Messenger.jsx
@@ -477,8 +477,8 @@ return(
                                     src={chatFriend.picture}
                                     className="object-cover w-full h-full" alt="avatar"/>
                             </div>
-                            <Link className="font-bold cursor-pointer c truncate w-36 block px-3 py-2 mx-1 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700" to={"/profile/"+chatFriend.id}>
-                                {chatFriend.userName}</Link>
+                            <div className="font-bold cursor-pointer c truncate w-36 block px-3 py-2 mx-1 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700" onClick={()=>navigate("/profile/"+chatFriend.id)}>
+                                {chatFriend.userName}</div>
 
                         </div>
 
